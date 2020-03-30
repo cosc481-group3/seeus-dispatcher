ff71e2fb670db22dee9d81c8eef8d54effcdee32
Nathan Johnson <n.johnson@criteo.com>
2020-03-30 02:18:35 -0400

    Rename index.js to dev.index.js and rename some package scripts

--- src/main/main.ts
+++ src/main/main.ts
@@ -1,7 +1,7 @@
 /**
  * Entry point of the Election app.
  */
-import { app, BrowserWindow } from 'electron';
+import {app, BrowserWindow} from 'electron';
 import * as path from 'path';
 import * as url from 'url';
 import fs from 'fs';
@@ -9,34 +9,34 @@ import fs from 'fs';
 let mainWindow: Electron.BrowserWindow | null;
 
 function createWindow(): void {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({
-    height: 600,
-    width: 800,
-    webPreferences: {
-      webSecurity: true,
-      allowRunningInsecureContent: false,
-      devTools: process.env.NODE_ENV !== 'production',
-    }
-  });
-  reloadWindowAfterBuild(mainWindow);
+    // Create the browser window.
+    mainWindow = new BrowserWindow({
+        height: 600,
+        width: 800,
+        webPreferences: {
+            webSecurity: true,
+            allowRunningInsecureContent: false,
+            devTools: process.env.NODE_ENV !== 'production',
+        }
+    });
+    reloadWindowAfterBuild(mainWindow);
 
-  // and load the index.html of the app.
-  mainWindow.loadURL(
-      url.format({
-        pathname: path.join(__dirname, './index.html'),
-        protocol: 'file:',
-        slashes: true
-      })
-  );
+    // and load the index.html of the app.
+    mainWindow.loadURL(
+        url.format({
+            pathname: path.join(__dirname, './index.html'),
+            protocol: 'file:',
+            slashes: true
+        })
+    );
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null;
-  });
+    // Emitted when the window is closed.
+    mainWindow.on('closed', () => {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null;
+    });
 }
 
 // This method will be called when Electron has finished
@@ -46,45 +46,50 @@ app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+    // On OS X it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
 });
 
 app.on('activate', () => {
-  // On OS X it"s common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow();
-  }
+    // On OS X it"s common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) {
+        createWindow();
+    }
 });
 
 function reloadWindowAfterBuild(window: BrowserWindow) {
-    if(process.env.NODE_ENV !== 'development') {
+    if (process.env.NODE_ENV !== 'development') {
         return;
     }
     let timeout: NodeJS.Timeout;
-    checkBundleFile().then(() => {
-        window.reload();
-    }).catch((err) => {
-        if(err.code === 'ENOENT') {
-            window.setTitle('Waiting for build to finish...');
-            clearTimeout(timeout);
-            timeout = setTimeout(() => reloadWindowAfterBuild(window), 1000);
-        } else {
-            console.error(err);
-        }
-    });
-}
 
-function checkBundleFile(filePath = './renderer.bundle.js') {
-    const bundlePath = path.resolve(__dirname, filePath);
-    return new Promise((resolve, reject) => {
-        fs.readFile(bundlePath, (err, data) => {
-            if(err) reject(err);
-            else resolve();
+    check();
+
+    function check() {
+        checkBundleFile().then(() => {
+            if (!window.isDestroyed()) window.reload();
+        }).catch((err) => {
+            clearTimeout(timeout);
+            if (err.code === 'ENOENT' && !window.isDestroyed()) {
+                window.setTitle('Waiting for build to finish...');
+                timeout = setTimeout(check, 1000);
+            } else {
+                console.error(err);
+            }
         });
-    });
+    }
+
+    function checkBundleFile(filePath = 'renderer.bundle.js') {
+        const bundlePath = path.resolve(__dirname, filePath);
+        return new Promise((resolve, reject) => {
+            fs.readFile(bundlePath, (err, data) => {
+                if (err) reject(err);
+                else resolve();
+            });
+        });
+    }
 }
--- webpack.main.js
+++ webpack.main.js
@@ -2,6 +2,8 @@ const CopyPlugin = require('copy-webpack-plugin');
 const baseConfig = require('./webpack.base');
 const merge = require('webpack-merge');
 
+const isDev = process.env.NODE_ENV === 'development';
+
 module.exports = merge(baseConfig, {
     entry: './src/main/main.ts',
     target: 'electron-main',
@@ -9,9 +11,9 @@ module.exports = merge(baseConfig, {
         filename: 'main.bundle.js'
     },
     plugins: [
-        new CopyPlugin([{
-            from: './src/main/index.js',
-            to: './main.index.js'
+        isDev && new CopyPlugin([{
+            from: './src/main/dev-index.js',
+            to: './main.dev-index.js'
         }])
     ]
 });
