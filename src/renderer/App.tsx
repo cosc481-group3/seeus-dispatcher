7f40497879a84db47872fdff379c84104dcc8e6f
Nathan Johnson <nathancj224@gmail.com>
2020-07-25 01:18:12 -0400

    App.tsx basic blueprint components

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import './App.css';
-import { Button, Intent } from '@blueprintjs/core';
+import { Alignment, Button, Intent, Navbar } from '@blueprintjs/core';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          SEEUS
-        </p>
-        <Button intent={Intent.SUCCESS} text="Close" onClick={window.close} />
-      </header>
-    </div>
+      <div className="App">
+          <Navbar>
+              <Navbar.Group align={Alignment.LEFT}>
+                  <Navbar.Heading>SEEUS</Navbar.Heading>
+                  <Navbar.Divider/>
+                  <Button minimal icon="list" text="Requests"/>
+              </Navbar.Group>
+              <Navbar.Group align={Alignment.RIGHT}>
+                  <Button minimal icon="cog" text="Settings"/>
+                  <Button minimal icon="log-out" text="Logout"/>
+              </Navbar.Group>
+          </Navbar>
+          <Button intent={Intent.SUCCESS} icon="plus">New Request</Button>
+      </div>
   );
 }
 
