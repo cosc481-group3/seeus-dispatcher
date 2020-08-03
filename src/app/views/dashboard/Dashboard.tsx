2edfbe91391c7a04699c5e0d62169daa4faa7759
Nathan Johnson <nathancj224@gmail.com>
2020-08-02 21:17:31 -0400

    Create Dashboard tab component

---
+++ src/app/views/dashboard/Dashboard.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Card, Table } from 'antd';
+
+const COLUMNS = [
+    { title: 'Location', dataIndex: 'location'},
+    { title: 'Destination', dataIndex: 'destination'},
+    { title: 'EID #', dataIndex: 'eid'},
+    { title: 'Name', dataIndex: 'name'},
+];
+
+export default class Dashboard extends React.Component<any, any> {
+    state = {
+        data: [
+            { location: 'test', destination: 'test', eid: 1361232, name: 'njohns48', id: 1 },
+        ]
+    }
+    render() {
+        return (
+            <Card title="Requests">
+                <Table dataSource={this.state.data} columns={COLUMNS} bordered={true} rowKey="id" />
+            </Card>
+        );
+    }
+}
